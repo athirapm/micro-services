548b453daec38e7413c0f233c4a80f21b789a38e
athirapm <athirapms@gmail.com>
2022-03-11 21:08:16 +0530

    service registry using eureka server

--- student-service/src/main/java/com/example/student/StudentServiceApplication.java
+++ student-service/src/main/java/com/example/student/StudentServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.student;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.student.feignclient")
+@EnableEurekaClient
 public class StudentServiceApplication {
 	@Value("${address.service.url}")
 	private String addressServiceUrl;
--- student-service/src/main/java/com/example/student/feignclient/AddressFeignClient.java
+++ student-service/src/main/java/com/example/student/feignclient/AddressFeignClient.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.student.dto.AddressResponse;
-
-@FeignClient(url = "${address.service.url}", value = "address-feign-client")
+// specifying the url and name of the address service
+//@FeignClient(url = "${address.service.url}", value = "address-feign-client")
+// specifying the name of the service as appeared in the eureka registry
+@FeignClient(value = "address-service")
 public interface AddressFeignClient {
 	@GetMapping("/fetch")
 	public AddressResponse fetchAddress(@RequestParam Long id);
